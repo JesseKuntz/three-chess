4975aff265d32fc987c5789c22125a8c39a1ebe5
Jesse Kuntz <jessekuntz96@gmail.com>
2019-04-23 17:14:29 -0400

    * populated chess board and added trackball

--- public/script.js
+++ public/script.js
@@ -2,48 +2,115 @@
 // BASIC SETUP
 // ------------------------------------------------
 
-var loader = new THREE.GLTFLoader();
+var camera, scene, renderer, controls, loader;
 
-// Create an empty scene
-var scene = new THREE.Scene();
+init();
+animate();
 
-// Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 4;
+function init() {
+	loader = new THREE.OBJLoader();
 
-// Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({antialias:true});
+	// Create an empty scene
+	scene = new THREE.Scene();
 
-// Configure renderer clear color
-renderer.setClearColor("#000000");
+	// Create a basic perspective camera
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1000);
+	camera.position.z = 8;
 
-// Configure renderer size
-renderer.setSize( window.innerWidth, window.innerHeight );
+	// Create a renderer with Antialiasing
+	renderer = new THREE.WebGLRenderer({antialias:true});
+	// Configure renderer clear color
+	renderer.setClearColor("#B9E0D9");
+	// Configure renderer size
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// Append Renderer to DOM
+	document.body.appendChild(renderer.domElement);
 
-// Append Renderer to DOM
-document.body.appendChild( renderer.domElement );
+	// Trackball
+	controls = new THREE.TrackballControls(camera);
+	controls.rotateSpeed = 3.0;
+	controls.zoomSpeed = 1.2;
+	controls.panSpeed = 0.8;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	controls.addEventListener('change', render);
 
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
+	window.addEventListener('resize', onWindowResize, false);
+	render();
 
-// Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-var cube = new THREE.Mesh( geometry, material );
+	// ------------------------------------------------
+	// FUN STARTS HERE
+	// ------------------------------------------------
 
-// Add cube to Scene
-scene.add( cube );
+	// load a the king -- doesn't work yet
+	// loader.load(
+	// 	// resource URL
+	// 	'models/king.obj',
+	// 	// called when resource is loaded
+	// 	function ( object ) {
+	// 		scene.add( object );
+	// 	},
+	// 	// called when loading is in progresses
+	// 	function ( xhr ) {
+	// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	// 	},
+	// 	// called when loading has errors
+	// 	function ( error ) {
+	// 		console.log( 'An error happened' );
+	// 	}
+	// );
 
-// Render Loop
-var render = function () {
-  requestAnimationFrame( render );
+	var geometry = new THREE.BoxGeometry(1, 1, 1);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+	let y = 3.5;
+	let color = "#ffffff"
+	for (let r = 0; r < 8; r++) {
+		let x = -3.5;
 
-  // Render the scene
-  renderer.render(scene, camera);
-};
+		for (let c = 0; c < 8; c++) {
+			let material = new THREE.MeshBasicMaterial({color: color});
+			let cube = new THREE.Mesh(geometry, material);
+			scene.add(cube);
 
-render();
+			cube.position.set(x, y, 0)
+			x++;
+
+			if (color === "#ffffff") color = "#000000";
+			else color = "#ffffff";
+		}
+
+		if (color === "#ffffff") color = "#000000";
+		else color = "#ffffff";
+		y--;
+	}
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	controls.update();
+	renderer.render(scene, camera);
+}
+
+function render() {
+	renderer.render(scene, camera);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// From https://github.com/mrdoob/three.js/blob/master/examples/misc_controls_trackball.html
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	controls.handleResize();
+	render();
+}
